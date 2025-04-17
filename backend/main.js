d53ac0fd00cf89932338b68146a1fe55a27a08fb
LuuTrongTan <luutrongtan232@gmail.com>
2025-04-17 08:44:18 +0700

    chuaw xong frontend

--- backend/main.js
+++ backend/main.js
@@ -12,7 +12,7 @@ app.use(cors());
 app.use(express.json());
 
 // Kết nối MongoDB
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGODB_URL, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log("MongoDB Connected"))
     .catch(err => console.error(err));
 
